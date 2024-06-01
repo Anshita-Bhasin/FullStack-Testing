e0b3c6d71ab57a60b9da3486265de58b5c4f493f
Anshita Bhasin <anshita.bhasin@gmail.com>
2024-06-01 17:48:14 +0400

    web api

--- API-Cypress/cypress/e2e/actions.cy.js
+++
@@ -1,7 +0,0 @@
-describe("API Test using cypress", () => {
-  it("test api - GET ", () => {
-    cy.request("GET", "https://reqres.in/api/users?page=2").should((response) => {
-      expect(response.status).to.eq(200);
-    });
-  });
-});
